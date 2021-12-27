035e2fb54fd7f5afa2961b9c661a78ba24d2ed5b
Ryan Erickson <rserickson92@gmail.com>
2021-12-27 14:08:12 -0500

    Potential solution for MeetingAvailability, need more tests

--- java/MeetingAvailability/MeetingAvailability.java
+++ java/MeetingAvailability/MeetingAvailability.java
@@ -1,10 +1,49 @@
+import java.util.*;
 
-public class Solution {
+public class MeetingAvailability {
 
-  public static boolean isAvailable(int[][] meetings, int meetingStart, int meetingEnd) {
+  static int[][] MEETINGS = new int[][] {
+    {1000, 1100},
+    {1400, 1500},
+    {1600, 1630}
+  };
+
+  static boolean isAvailable(int[][] meetings, int availableStart, int availableEnd) {
+    for (int[] meeting : meetings) {
+      int meetingStart = meeting[0];
+      int meetingEnd = meeting[1];
+      for (int minute = availableStart; minute < availableEnd; minute++) {
+	if (minute >= meetingStart && minute < meetingEnd) {
+	  System.out.println(String.format("Failing minute=%d: meeting=[%d, %d]", minute, meetingStart, meetingEnd));
+	  return false;
+	}
+      }
+    }
+
+    return true;
+  }
+
+  static void runIsAvailable(int[][] meetings, int availableStart, int availableEnd, boolean expected) {
+    System.out.println(String.format("Available from %d to %d? Expected=%s Actual=%s",
+      availableStart, availableEnd, expected, isAvailable(meetings, availableStart, availableEnd)));
+    System.out.println();
+  }
+
+  static void printMeetings(int[][] meetings) {
+    for (int[] meeting : meetings) {
+      System.out.print(String.format("[%d, %d] ", meeting[0], meeting[1]));
+    }
+    System.out.println();
   }
 
   public static void main(String[] args) {
+    printMeetings(MEETINGS);
+    runIsAvailable(MEETINGS, 900, 930, true);
+    runIsAvailable(MEETINGS, 900, 1030, false);
+    runIsAvailable(MEETINGS, 900, 1400, false);
+    runIsAvailable(MEETINGS, 1500, 1600, true);
+    runIsAvailable(MEETINGS, 1359, 1600, false);
+    runIsAvailable(MEETINGS, 1400, 1430, false);
   }
 }
 
